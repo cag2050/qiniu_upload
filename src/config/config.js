df5291727e10e36191b35c4e58737f7d40956d12
cag2050 <cag2050@163.com>
2017-04-26 17:22:22 +0800

    m

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
     bucket_name: 'testvideo2',
     domain: 'http://oodr8juo6.bkt.clouddn.com/',
-    uptoken: 'iUTbwTRLotclpFa8kHoeSUvgxgvH1WL2-ROdbY7B:ouSzE01U4G8M9txwtrxyUqjSLXs=:eyJzY29wZSI6InRlc3R2aWRlbzIiLCJkZWFkbGluZSI6MTQ5MzExOTU1NH0='
+    uptoken: 'iUTbwTRLotclpFa8kHoeSUvgxgvH1WL2-ROdbY7B:MbZW4Je_lLvSeZ4nhuqg_h7tJfg=:eyJzY29wZSI6InRlc3R2aWRlbzIiLCJkZWFkbGluZSI6MTQ5MzE3NjE3Mn0='
 }
